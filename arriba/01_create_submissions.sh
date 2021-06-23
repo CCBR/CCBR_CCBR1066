f9ff67a51433f63be159ae1d2f25372ef3cad37c
slsevilla <slsevilla@gmail.com>
2021-06-23 12:17:57 -0400

    remove arriba dri

--- arriba/01_create_submissions.sh
+++
@@ -1,4 +0,0 @@
-sra_file="/data/sevillas2/arriba/refs/sra_files_unique.txt"
-
-awk '{ print "fasterq-dump " $1 " -O /data/sevillas2/arriba/fastq -t /data/sevillas2/arriba/fastq" }' $sra_file > /home/sevillas2/git/Pipeliner/testing/submit_fq.slurm
-awk '{ print "gzip /data/sevillas2/arriba/fastq/" $1 "_1.fastq \n gzip /data/sevillas2/arriba/fastq/" $1 "_2.fastq" }' $sra_file > /home/sevillas2/git/Pipeliner/testing/submit_gz.slurm
